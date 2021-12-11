5b3a3db60ab5315ca4cf4c916970ce5e13a6430d
Patxi Bocos <patxi952@gmail.com>
2021-12-11 09:33:27 +0100

    Read test

--- alexa-skill-lambda/main.go
+++ alexa-skill-lambda/main.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"fmt"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/patxibocos/alexa-cycling-skill/alexa-skill-lambda/pcsscraper"
+	"google.golang.org/protobuf/proto"
 	"io/ioutil"
 	"net/http"
 )
@@ -15,7 +17,9 @@ func HandleRequest(ctx context.Context) (string, error) {
 		whatever = fmt.Sprintf("error: %s", err)
 	} else {
 		bytes, _ := ioutil.ReadAll(response.Body)
-		whatever = string(bytes)
+		cyclingData := new(pcsscraper.CyclingData)
+		_ = proto.Unmarshal(bytes, cyclingData)
+		whatever = cyclingData.Riders[0].LastName
 	}
 	return fmt.Sprintf("Hi there %s", whatever), nil
 }
