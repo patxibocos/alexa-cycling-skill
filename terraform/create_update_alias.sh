efeb3b460cd71e39875658db36c5cb86171b1db2
Patxi Bocos <patxi952@gmail.com>
2022-01-10 22:21:28 +0100

    test

--- terraform/create_update_alias.sh
+++ terraform/create_update_alias.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
-GIT_HASH=$(git rev-parse --short "$GITHUB_SHA")
+ALIAS_NAME=$1
 
 ALEXA_CYCLING_LAMBDA_VERSION=$(terraform output -raw alexa_cycling_lambda_version)
 ALEXA_CYCLING_LAMBDA_NAME=$(terraform output -raw alexa_cycling_lambda_name)
 
 aws lambda update-alias \
   --function-name $ALEXA_CYCLING_LAMBDA_NAME \
-  --name $GIT_HASH \
+  --name $ALIAS_NAME \
   --function-version $ALEXA_CYCLING_LAMBDA_VERSION \
   > /dev/null \
 || \
 aws lambda create-alias \
   --function-name $ALEXA_CYCLING_LAMBDA_NAME \
-  --name $GIT_HASH \
+  --name $ALIAS_NAME \
   --function-version $ALEXA_CYCLING_LAMBDA_VERSION \
-  --description "Build from $GIT_HASH commit"
   > /dev/null
