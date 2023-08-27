e445c9e5ea13e7146bceb81b5156f0aef7b7108a
Patxi Bocos <patxibocos@lyft.com>
2023-08-27 15:54:58 -0700

    Remove year from raceIDToName map

--- alexa-skill-lambda/internal/alexa/format.go
+++ alexa-skill-lambda/internal/alexa/format.go
@@ -23,42 +23,42 @@ var (
 		"December", "Diciembre")
 
 	raceIDToName = map[string]string{
-		"tour-down-under-2023":                  "El Santos Tour Down Under",
-		"great-ocean-race-2023":                 "La Cadel Evans Great Ocean",
-		"uae-tour-2023":                         "El U A E Tour",
-		"omloop-het-nieuwsblad-2023":            "La Omloop Het Nieuwsblad",
-		"strade-bianche-2023":                   "La Strade Bianche",
-		"paris-nice-2023":                       "La París Niza",
-		"tirreno-adriatico-2023":                "El Tirreno Adriático",
-		"milano-sanremo-2023":                   "La Milán San Remo",
-		"volta-a-catalunya-2023":                "La Volta a Cataluña",
-		"oxyclean-classic-brugge-de-panne-2023": "La Clásica Brujas La Panne",
-		"e3-harelbeke-2023":                     "La Clásica E3 Saxo Bank",
-		"gent-wevelgem-2023":                    "La Gante Wevelgem",
-		"dwars-door-vlaanderen-2023":            "La Clásica A Través de Flandes",
-		"ronde-van-vlaanderen-2023":             "El Tour de Flandes",
-		"itzulia-basque-country-2023":           "La Vuelta al País Vasco",
-		"paris-roubaix-2023":                    "La París Rubé",
-		"amstel-gold-race-2023":                 "La Amstel Gold Race",
-		"la-fleche-wallone-2023":                "La Flecha Valona",
-		"liege-bastogne-liege-2023":             "La Lieja Bastoña Lieja",
-		"tour-de-romandie-2023":                 "El Tour de Romandía",
-		"Eschborn-Frankfurt-2023":               "La Eschborn Frankfurt",
-		"giro-d-italia-2023":                    "El Yiro de Italia",
-		"dauphine-2023":                         "El Critérium del Dofiné",
-		"tour-de-suisse-2023":                   "El Tour de Suiza",
-		"tour-de-france-2023":                   "El Tour de Francia",
-		"san-sebastian-2023":                    "La Clásica San Sebastián",
-		"tour-de-pologne-2023":                  "El Tour de Polonia",
-		"cyclassics-hamburg-2023":               "La Clásica Bemer",
-		"renewi-tour-2023":                      "El Tour de Renewi",
-		"benelux-tour-2023":                     "El Tour del Benelux",
-		"vuelta-a-espana-2023":                  "La Vuelta a España",
-		"bretagne-classic-2023":                 "La Clásica Bretaña",
-		"gp-quebec-2023":                        "La Clásica Quebec",
-		"gp-montreal-2023":                      "La Clásica Montreal",
-		"il-lombardia-2023":                     "El Giro de Lombardía",
-		"tour-of-guangxi-2023":                  "El Tour de Guangxi",
+		"tour-down-under":                  "El Santos Tour Down Under",
+		"great-ocean-race":                 "La Cadel Evans Great Ocean",
+		"uae-tour":                         "El U A E Tour",
+		"omloop-het-nieuwsblad":            "La Omloop Het Nieuwsblad",
+		"strade-bianche":                   "La Strade Bianche",
+		"paris-nice":                       "La París Niza",
+		"tirreno-adriatico":                "El Tirreno Adriático",
+		"milano-sanremo":                   "La Milán San Remo",
+		"volta-a-catalunya":                "La Volta a Cataluña",
+		"oxyclean-classic-brugge-de-panne": "La Clásica Brujas La Panne",
+		"e3-harelbeke":                     "La Clásica E3 Saxo Bank",
+		"gent-wevelgem":                    "La Gante Wevelgem",
+		"dwars-door-vlaanderen":            "La Clásica A Través de Flandes",
+		"ronde-van-vlaanderen":             "El Tour de Flandes",
+		"itzulia-basque-country":           "La Vuelta al País Vasco",
+		"paris-roubaix":                    "La París Rubé",
+		"amstel-gold-race":                 "La Amstel Gold Race",
+		"la-fleche-wallone":                "La Flecha Valona",
+		"liege-bastogne-liege":             "La Lieja Bastoña Lieja",
+		"tour-de-romandie":                 "El Tour de Romandía",
+		"Eschborn-Frankfurt":               "La Eschborn Frankfurt",
+		"giro-d-italia":                    "El Yiro de Italia",
+		"dauphine":                         "El Critérium del Dofiné",
+		"tour-de-suisse":                   "El Tour de Suiza",
+		"tour-de-france":                   "El Tour de Francia",
+		"san-sebastian":                    "La Clásica San Sebastián",
+		"tour-de-pologne":                  "El Tour de Polonia",
+		"cyclassics-hamburg":               "La Clásica Bemer",
+		"renewi-tour":                      "El Tour de Renewi",
+		"benelux-tour":                     "El Tour del Benelux",
+		"vuelta-a-espana":                  "La Vuelta a España",
+		"bretagne-classic":                 "La Clásica Bretaña",
+		"gp-quebec":                        "La Clásica Quebec",
+		"gp-montreal":                      "La Clásica Montreal",
+		"il-lombardia":                     "El Giro de Lombardía",
+		"tour-of-guangxi":                  "El Tour de Guangxi",
 	}
 )
 
@@ -71,5 +71,8 @@ func formattedDate(time time.Time) string {
 }
 
 func raceName(raceID string) string {
-	return raceIDToName[raceID]
+	// raceID contains -YYYY so we remove it to calculate map's key
+	lastDashIndex := strings.LastIndex(raceID, "-")
+	mapKey := raceID[:lastDashIndex]
+	return raceIDToName[mapKey]
 }
