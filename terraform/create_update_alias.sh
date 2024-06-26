917c6183731e0226b559bf24652ae369fc124591
Patxi Bocos <patxibocos@lyft.com>
2024-06-26 10:48:57 -0700

    Add alexa permission to alias

--- terraform/create_update_alias.sh
+++ terraform/create_update_alias.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 ALIAS_NAME=$1
+ALEXA_EVENT_SOURCE_TOKEN=$2
 
 ALEXA_CYCLING_LAMBDA_VERSION=$(terraform output -raw alexa_cycling_lambda_version)
 ALEXA_CYCLING_LAMBDA_NAME=$(terraform output -raw alexa_cycling_lambda_name)
@@ -14,4 +15,12 @@ aws lambda create-alias \
   --function-name $ALEXA_CYCLING_LAMBDA_NAME \
   --name $ALIAS_NAME \
   --function-version $ALEXA_CYCLING_LAMBDA_VERSION \
+  > /dev/null
+aws lambda add-permission \
+  --function-name $ALEXA_CYCLING_LAMBDA_NAME \
+  --statement-id "AllowExecutionFromAlexa" \
+  --action "lambda:InvokeFunction" \
+  --principal "alexa-appkit.amazon.com" \
+  --qualifier $ALIAS_NAME \
+  --event-source-token $ALEXA_EVENT_SOURCE_TOKEN \
   > /dev/null
