2beb707478bac23a2133c7af2df81c5b1925b327
Patxi Bocos <patxibocos@lyft.com>
2024-06-26 20:44:36 -0700

    Escape alias name

--- terraform/create_update_alias.sh
+++ terraform/create_update_alias.sh
@@ -7,13 +7,13 @@ ALEXA_CYCLING_LAMBDA_NAME=$(terraform output -raw alexa_cycling_lambda_name)
 
 aws lambda update-alias \
   --function-name $ALEXA_CYCLING_LAMBDA_NAME \
-  --name $ALIAS_NAME \
+  --name "$ALIAS_NAME" \
   --function-version $ALEXA_CYCLING_LAMBDA_VERSION \
   > /dev/null \
 || \ # Try update, otherwise update
 aws lambda create-alias \
   --function-name $ALEXA_CYCLING_LAMBDA_NAME \
-  --name $ALIAS_NAME \
+  --name "$ALIAS_NAME" \
   --function-version $ALEXA_CYCLING_LAMBDA_VERSION \
   > /dev/null
 aws lambda add-permission \
@@ -21,6 +21,6 @@ aws lambda add-permission \
   --statement-id "AllowExecutionFromAlexa" \
   --action "lambda:InvokeFunction" \
   --principal "alexa-appkit.amazon.com" \
-  --qualifier $ALIAS_NAME \
+  --qualifier "$ALIAS_NAME" \
   --event-source-token $ALEXA_EVENT_SOURCE_TOKEN \
   > /dev/null
